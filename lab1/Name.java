a1dbf16e0fa7af50bb1626ba2d0460ddf19d8f68
10Keshav <keshavananth06@gmail.com>
2025-12-12 13:55:25 +0530

    expt 5 added

---
+++ lab1/Name.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Name {
+  public static void main(String[] args) {
+    Scanner obj = new Scanner(System.in);
+    System.out.print("fname: ");
+    String fname = obj.nextLine();
+    System.out.print("lname: ");
+    String lname = obj.nextLine();
+    System.out.println(lname + " " + fname);
+    obj.close();
+  }
+}
