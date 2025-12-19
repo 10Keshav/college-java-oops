03eec5447a01b5100c7530f81624cca4fd65a332
10Keshav <keshavananth06@gmail.com>
2025-12-19 13:33:49 +0530

    added overload programme

---
+++ lab2/Overload.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class Overloading {
+  int sub(int a, int b) {
+    return a-b;
+  }
+
+  double sub(double a, double b) {
+    return a-b;
+  }
+
+  long sub(long a, long b) {
+    return a-b;
+  } 
+} 
+
+class Driver {
+  public static void main(String[] args) {
+   Overloading o = new Overloading();
+   System.out.println(o.sub(4,5));
+   System.out.println(o.sub(4.2,5.51));
+   System.out.println(o.sub(4132768,57281394));
+  }
+} 
--- lab2/countOddEven.java
+++ lab2/countOddEven.java
@@ -2,11 +2,7 @@ public class countOddEven {
   public static void main(String[] args) {
     int oddC = 0;
     int evenC = 0;
-    // if (args.length != 10) {
-    // System.out.println("enter 10");
-    // return;
-    // }
-    for (int i = 0; i < 10; i++) {
+   for (int i = 0; i < 10; i++) {
       int num = Integer.parseInt(args[i]);
       if (num % 2 == 0)
         evenC++;
